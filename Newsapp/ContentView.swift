e90f0ecaf04d04ce2b45976a73292a899b5c6f9d
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2020-07-25 08:43:14 -1000

    Initial Commit

---
+++ Newsapp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Newsapp
+//
+//  Created by Meghdad Abbaszadegan on 7/25/20.
+//  Copyright © 2020 Meghdad Abbaszadegan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
