bad2cd3cba815dbb1c2fd39dab9f8e5124d7b11c
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2020-07-25 09:40:59 -1000

    set NetworkManager

--- Newsapp/ContentView.swift
+++ Newsapp/ContentView.swift
@@ -9,12 +9,18 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var networkManager = NetworkManager()
+    
     var body: some View {
         NavigationView {
-            List(posts) { post in
+            List(networkManager.posts) { post in
                 Text(post.title)
             }
             .navigationBarTitle("News")
+        }
+        .onAppear {
+            self.networkManager.fetchData()
         }
     }
 }
@@ -26,13 +32,13 @@ struct ContentView_Previews: PreviewProvider {
 }
 
 
-struct Post: Identifiable {
-    let id: String
-    let title: String
-}
+//struct Post: Identifiable {
+//    let id: String
+//    let title: String
+//}
 
-let posts = [
-    Post(id: "1", title: "Post 1"),
-    Post(id: "2", title: "Post 2"),
-    Post(id: "3", title: "Post 3")
-]
+//let posts = [
+//    Post(id: "1", title: "Post 1"),
+//    Post(id: "2", title: "Post 2"),
+//    Post(id: "3", title: "Post 3")
+//]
---
+++ Newsapp/Models/PostData.swift
@@ -0,0 +1,23 @@
+//
+//  PostData.swift
+//  Newsapp
+//
+//  Created by Meghdad Abbaszadegan on 7/25/20.
+//  Copyright © 2020 Meghdad Abbaszadegan. All rights reserved.
+//
+
+import Foundation
+
+struct Results: Decodable {
+    let hits: [Post]
+}
+
+struct Post: Decodable, Identifiable {
+    var id: String {
+        return objectID
+    }
+    let objectID: String
+    let title: String
+    let points: Int
+    let url: String?
+}
