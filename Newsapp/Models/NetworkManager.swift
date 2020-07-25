05712aaad4e055b0e6e22fb08b03238ffa23e8bc
Meghdad Abbaszadegan <m3ghdad@gmail.com>
2020-07-25 09:00:10 -1000

    News view initialized

---
+++ Newsapp/Models/NetworkManager.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkManager.swift
+//  Newsapp
+//
+//  Created by Meghdad Abbaszadegan on 7/25/20.
+//  Copyright © 2020 Meghdad Abbaszadegan. All rights reserved.
+//
+
+import Foundation
+
